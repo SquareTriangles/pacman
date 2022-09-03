13d11772aeaac88a1b82aedac069885aa1c4b394
boris <borislakushev@yandex.ru>
2022-09-03 16:28:08 +0300

    create: loader

---
+++ packages/client/src/components/StartLoader/StartLoader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './styles.module.css';
+
+const LOADER_TEXT = 'lOAdIng ...'
+
+const StartLoader: React.FC = () => {
+  return (
+    <div className={styles.loader}>
+      <p className={styles.text}>{LOADER_TEXT}</p>
+      <div className={styles.polling} />
+    </div>
+  );
+}
+
+export default StartLoader;
--- packages/client/src/pages/Leaderboard/Leaderboard.tsx
+++ packages/client/src/pages/Leaderboard/Leaderboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BasicTable from './components/table/Table';
 import Header from './components/Header/Header';
+import StartLoader from '../../components/StartLoader/StartLoader';
 import styles from './styles.module.css';
 
 const HEADER_TEXT = 'Pacman Leaders';
