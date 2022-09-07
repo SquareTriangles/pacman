101ad968c71765de907c58cf0cb8342592e7bcf0
boris <borislakushev@yandex.ru>
2022-09-07 07:20:06 +0300

    fix: store

--- packages/client/src/redux/store.ts
+++ packages/client/src/redux/store.ts
@@ -1,13 +1,18 @@
 import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
 import ForumReducer from './reducers/ForumReducer'
 
-const store = combineReducers({
+const rootReducer = combineReducers({
   ForumReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
 })
 
 // @ts-ignore
 window.__store__ = store
 
-export type RootState = ReturnType<typeof store>
+export type RootState = ReturnType<typeof rootReducer>
 
 export default store
