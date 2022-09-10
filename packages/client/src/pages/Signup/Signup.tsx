cb24b6d4bc2bc15153fade23a1b4ce4877e369a1
Aleksey <enh1260@gmail.com>
2022-09-10 22:55:58 +0300

    add signup page and api

--- packages/client/src/pages/Signup/Signup.tsx
+++ packages/client/src/pages/Signup/Signup.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import SignupForm from './components/SignupForm'
 
 const Signup: React.FC = () => {
-  return <div>Signup page</div>
+  return (
+    <Container maxWidth="xs">
+      <Typography textAlign="center" variant="h5">Регистрация</Typography>
+      <SignupForm></SignupForm>
+    </Container>
+  )
 }
 
 export default Signup
