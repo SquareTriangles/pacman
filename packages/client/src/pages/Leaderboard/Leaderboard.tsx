f086de1b0dfa046fe2294b44f58b4cf80362b34f
boris <borislakushev@yandex.ru>
2022-09-17 11:16:54 +0300

    create adaptive for leaderboard

--- packages/client/src/pages/Leaderboard/Leaderboard.tsx
+++ packages/client/src/pages/Leaderboard/Leaderboard.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import BasicTable from './components/table/Table'
 import Header from './components/Header/Header'
-import StartLoader from '../../components/StartLoader/StartLoader'
 import styles from './styles.module.css'
 
 const HEADER_TEXT = 'Pacman Leaders'
 
 const Leaderboard: React.FC = () => {
   return (
-    <div className={styles.page}>
+    <div className={styles.leaderboard}>
       <Header text={HEADER_TEXT} />
       <BasicTable />
     </div>
