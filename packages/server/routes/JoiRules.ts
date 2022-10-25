b949af04b8059ae71a367f646cf6e758935337e3
boris <borislakushev@yandex.ru>
2022-10-25 20:47:27 +0300

    add JoiRules file

---
+++ packages/server/routes/JoiRules.ts
@@ -0,0 +1,9 @@
+import { Joi } from 'celebrate'
+import { stringErrorMessageBuilder } from '../errors'
+
+export const idRule        = Joi.string().guid({version: ['uuidv4']}).required().messages(stringErrorMessageBuilder('id'));
+export const FirstNameRule = Joi.string().min(5).max(32).required().messages(stringErrorMessageBuilder('firstName'));
+export const LastNameRule  = Joi.string().min(5).max(32).required().messages(stringErrorMessageBuilder('lastName'));
+export const AvatarRule    = Joi.string().required().uri().messages(stringErrorMessageBuilder('avatar'));
+export const EmailRule     = Joi.string().required().email().messages(stringErrorMessageBuilder('email'));
+export const LoginRule     = Joi.string().min(5).max(32).required().messages(stringErrorMessageBuilder('login'));
--- packages/server/routes/user.ts
+++ packages/server/routes/user.ts
@@ -4,35 +4,28 @@ import {
   updateUserController,
 } from '../controlers/user'
 import router from 'express';
-import { stringErrorMessageBuilder } from '../errors'
+import * as JoiRules from './JoiRules';
 
 const Router = router.Router()
 
-const idRule        = Joi.string().guid({version: ['uuidv4']}).required().messages(stringErrorMessageBuilder('id'));
-const FirstNameRule = Joi.string().min(5).max(32).required().messages(stringErrorMessageBuilder('firstName'));
-const LastNameRule  = Joi.string().min(5).max(32).required().messages(stringErrorMessageBuilder('lastName'));
-const AvatarRule    = Joi.string().required().uri().messages(stringErrorMessageBuilder('avatar'));
-const EmailRule     = Joi.string().required().email().messages(stringErrorMessageBuilder('email'));
-const LoginRule     = Joi.string().min(5).max(32).required().messages(stringErrorMessageBuilder('login'));
-
 Router.post('/', celebrate({
   body: Joi.object().keys({
-    firstName: FirstNameRule,
-    lastName: LastNameRule,
-    avatar: AvatarRule,
-    email: EmailRule,
-    login: LoginRule,
+    firstName: JoiRules.FirstNameRule,
+    lastName: JoiRules.LastNameRule,
+    avatar: JoiRules.AvatarRule,
+    email: JoiRules.EmailRule,
+    login: JoiRules.LoginRule,
   })
 }), createUserController);
 
 Router.put('/', celebrate({
   body: Joi.object().keys({
-    id: idRule,
-    firstName: FirstNameRule,
-    lastName: LastNameRule,
-    avatar: AvatarRule,
-    email: EmailRule,
-    login: LoginRule,
+    id: JoiRules.idRule,
+    firstName: JoiRules.FirstNameRule,
+    lastName: JoiRules.LastNameRule,
+    avatar: JoiRules.AvatarRule,
+    email: JoiRules.EmailRule,
+    login: JoiRules.LoginRule,
   })
 }), updateUserController);
 
