b5026064dffb04f9c2b515dee82ae5f20aacc2c1
boris <borislakushev@yandex.ru>
2022-10-29 14:28:55 +0300

    add: cors origin

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -13,13 +13,19 @@ const port = Number(process.env.SERVER_PORT) || 3001
 
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 
+}
+
+app.use(consoleLogger)
+
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.disable('x-powered-by');
 
-app.use(consoleLogger)
 
 dbConnect()
 
