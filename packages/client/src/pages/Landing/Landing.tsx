4ef5798c4f8b8b707787f459635324ed24ffdbd7
boris <borislakushev@yandex.ru>
2022-11-09 21:54:28 +0300

    add info on about page

--- packages/client/src/pages/Landing/Landing.tsx
+++ packages/client/src/pages/Landing/Landing.tsx
@@ -1,29 +1,88 @@
 import React from "react";
 import { Container } from "@mui/material";
-import { Link } from "react-router-dom";
 import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
-import * as routeList from '../../utils/Routes';
+import { Avatar, Divider, Grid } from "@mui/material";
+import styles from './styles.module.css'
 
-const Landing: React.FC = () => {
-    return (
-        <Container>
-            <Typography sx={{
-                textAlign: "center",
-                marginBottom: '150px'
-            }} variant="h1" component="h1">
-                Packman
-            </Typography>
-            <Stack spacing={2}
-                sx={{
-                    justifyContent: "center"
-                }}
-                direction="row">
-                <Link className="landing__link_signin" to={routeList.SIGNIN_ROUTE}>Войти</Link>
-                <Link className="landing__link_signup" to={routeList.SIGNUP_ROUTE}>Зарегистрироваться</Link>
-            </Stack>
-        </Container>
-    )
+
+import pacman_image from '../../assets/images/pac-man.webp'
+import react_image from '../../assets/images/React.png'
+import docker_image from '../../assets/images/docker.png'
+import expressjs_image from '../../assets/images/Expressjs.png'
+import git_image from '../../assets/images/git.png'
+import nodejs_image from '../../assets/images/nodejs.png'
+import oauth_image from '../../assets/images/oauth.png'
+import redux_image from '../../assets/images/redux.png'
+
+type TTecnology = {
+    image: string,
+    alt: string,
 }
+
+const Tecnology: React.FC<TTecnology> = ({ image, alt }) => (
+    <Grid item xs="auto">
+        <Avatar
+            src={image}
+            alt={alt}
+            className={styles.image}
+            sx={{
+                width: '100px',
+                height: '100px',
+            }} />
+    </Grid>)
+
+
+
+const Landing: React.FC = () => (
+    <Container
+        sx={{
+            marginTop: '50px'
+        }}
+    >
+        <div className={styles.row}>
+            <Avatar
+                src={pacman_image}
+                variant="square"
+                alt='pacman'
+                sx={{
+                    width: '100px',
+                    height: '100px',
+                }} />
+            <Typography
+                component="span"
+                sx={{
+                    marginLeft: '20px',
+                    fontSize: '28px',
+                    fontFamily: 'Inter',
+                }}
+            >
+                Наша команда <span style={{ fontWeight: 800 }}> Square-triangles </span>
+                из 17 когорты Яндекс Практикума.В поте лица разрабатывает всем известную игру
+                <span style={{ fontWeight: 800 }}> Pacman</span>.
+            </Typography>
+        </div>
+        <Typography
+            component="h3"
+            sx={{
+                fontSize: '28px',
+                fontFamily: 'Inter',
+                fontWeight: 800,
+                marginTop: '50px',
+            }}
+        >
+            Используемые технологии
+        </Typography>
+        <Grid container spacing={3}
+        >
+            <Tecnology image={react_image} alt={'react'} />
+            <Tecnology image={redux_image} alt={'redux'} />
+            <Tecnology image={nodejs_image} alt={'nodejs'} />
+            <Tecnology image={expressjs_image} alt={'expressjs'} />
+            <Tecnology image={oauth_image} alt={'oauth'} />
+            <Tecnology image={docker_image} alt={'docker'} />
+            <Tecnology image={git_image} alt={'git'} />
+        </Grid>
+    </Container>
+)
 
 export default Landing
--- packages/client/src/pages/Landing/index.ts
+++ packages/client/src/pages/Landing/index.ts
@@ -1,2 +1,1 @@
-import './styles.modul.css';
 export { default } from './Landing'
